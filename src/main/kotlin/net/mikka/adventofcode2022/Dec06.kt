3cd2a5c9c2c33d620a39834fbd6e18c016ebe204
Michael Fritsch <michael.fritsch@holisticon.de>
2022-12-06 22:39:27 +0100

    day 6

---
+++ src/main/kotlin/net/mikka/adventofcode2022/Dec06.kt
@@ -0,0 +1,66 @@
+package net.mikka.adventofcode2022
+
+@Suppress("unused")
+class Dec06 : PuzzleDay<Int>() {
+
+    override fun getPuzzle1(): Puzzle<Int> {
+        return Puzzle01()
+    }
+
+    override fun getPuzzle2(): Puzzle<Int> {
+        return Puzzle02()
+    }
+
+    companion object {
+     
+    }
+
+    class Puzzle01 : Puzzle<Int> {
+        override fun solve(input: List<String>): Int {
+
+            var index = 0
+            var charWindow = ""
+            for (cc in input.first().chars()) {
+                val c = cc.toChar().toString()
+
+                if (charWindow.length < 4) {
+                    charWindow += c
+                } else {
+                    charWindow = charWindow.substring(1) + c
+
+                    if (charWindow.chars().distinct().count() == charWindow.chars().count()) {
+                        return index + 1
+                    }
+                }
+
+                index++
+            }
+
+            throw IllegalStateException()
+        }
+    }
+
+    class Puzzle02 : Puzzle<Int> {
+        override fun solve(input: List<String>): Int {
+            var index = 0
+            var charWindow = ""
+            for (cc in input.first().chars()) {
+                val c = cc.toChar().toString()
+
+                if (charWindow.length < 14) {
+                    charWindow += c
+                } else {
+                    charWindow = charWindow.substring(1) + c
+
+                    if (charWindow.chars().distinct().count() == charWindow.chars().count()) {
+                        return index + 1
+                    }
+                }
+
+                index++
+            }
+
+            throw IllegalStateException()
+        }
+    }
+}
---
+++ src/test/kotlin/net/mikka/adventofcode2022/Dec06Test.kt
@@ -0,0 +1,13 @@
+package net.mikka.adventofcode2022
+
+internal class Dec06Test : AbstractAdventOfCodeTest<Int>() {
+
+    override fun getInput(): String {
+        return """mjqjpqmgbljsphdztnvjfqwrcgsmlb"""
+    }
+
+    override fun getExpectedValueForPuzzle1() = 7
+
+    override fun getExpectedValueForPuzzle2() = 19
+
+}
